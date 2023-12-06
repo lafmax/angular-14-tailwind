35353f631f8e2428a2225ae626c130acd8650fce
lafmax <lafmaxdev@gmail.com>
2023-12-06 10:23:20 -0300

    segundo commit

--- user-store.repository.ts
+++ user-store.repository.ts
@@ -8,7 +8,7 @@ export interface UserStoreProps {
     }
 }
 
-export const store = createStore({ name: 'userStore' }, withProps<UserStoreProps>({id: 2, data: {nombre: 'jaime', edad: 34} }));
+export const store = createStore({ name: 'userStore' }, withProps<UserStoreProps>({id: 2, data: {nombre: 'jaime', edad: 33} }));
 
 export const user = store.pipe(select((state) => state.data));
 
